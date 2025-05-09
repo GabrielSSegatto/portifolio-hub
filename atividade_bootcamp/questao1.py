5b08bf023420c7989e1b03558766cc283959811b
Pedro Miguel  silva <pedromsgabeto@gmail.com>
2025-05-08 21:20:09 -0300

    falta o int no input
    o codigo está bom mas faltou receber o input para int e sair da string

--- atividade_bootcamp/questao1.py
+++ atividade_bootcamp/questao1.py
@@ -1,6 +1,6 @@
 '''Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.'''
 
-num = (input('Digite um número: '))
+num = int(input('Digite um número: ')))
 sucessor = num + 1
 antecessor = num - 1
 print(f'O sucessor de {num} é {sucessor} e o antecessor é {antecessor}.')
